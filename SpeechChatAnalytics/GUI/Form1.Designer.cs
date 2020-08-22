9db2a811fc0b8191b4924f81a85067185616cee8
Alexey Fedotov <alexeyfedotov1998@gmail.com>
2020-08-22 18:57:39 +0300

    Rewrited from scratch. Changed structure of programm, I tried use MVC

--- SpeechChatAnalytics/GUI/Form1.Designer.cs
+++ SpeechChatAnalytics/GUI/Form1.Designer.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param Name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -30,86 +30,75 @@
         {
             this.buttonRead = new System.Windows.Forms.Button();
             this.textBoxDirectionForReading = new System.Windows.Forms.TextBox();
-            this.textBoxDirectionForWritting = new System.Windows.Forms.TextBox();
-            this.buttonWrite = new System.Windows.Forms.Button();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressBarWriting = new System.Windows.Forms.ProgressBar();
             this.richTextBoxAllThemes = new System.Windows.Forms.RichTextBox();
             this.richTextBoxSelectedThemes = new System.Windows.Forms.RichTextBox();
             this.checkBoxAllowChangeThemes = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonChooseReadingDirection = new System.Windows.Forms.Button();
-            this.buttonChooseWrittingDirection = new System.Windows.Forms.Button();
             this.buttonSaveThemes = new System.Windows.Forms.Button();
+            this.labelWritingProgress = new System.Windows.Forms.Label();
+            this.progressBarAnalyze = new System.Windows.Forms.ProgressBar();
+            this.labelAnalyzeProgress = new System.Windows.Forms.Label();
+            this.progressBarReading = new System.Windows.Forms.ProgressBar();
+            this.labelReadingProgress = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonRead
             // 
             this.buttonRead.Enabled = false;
-            this.buttonRead.Location = new System.Drawing.Point(12, 421);
+            this.buttonRead.Location = new System.Drawing.Point(16, 435);
+            this.buttonRead.Margin = new System.Windows.Forms.Padding(4);
             this.buttonRead.Name = "buttonRead";
-            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.Size = new System.Drawing.Size(100, 28);
             this.buttonRead.TabIndex = 0;
             this.buttonRead.Text = "Выгрузить";
             this.buttonRead.UseVisualStyleBackColor = true;
-            this.buttonRead.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonRead_MouseClick);
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
             // 
             // textBoxDirectionForReading
             // 
-            this.textBoxDirectionForReading.Location = new System.Drawing.Point(93, 395);
+            this.textBoxDirectionForReading.Location = new System.Drawing.Point(124, 391);
+            this.textBoxDirectionForReading.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxDirectionForReading.Name = "textBoxDirectionForReading";
-            this.textBoxDirectionForReading.Size = new System.Drawing.Size(210, 20);
+            this.textBoxDirectionForReading.Size = new System.Drawing.Size(279, 22);
             this.textBoxDirectionForReading.TabIndex = 1;
             this.textBoxDirectionForReading.Text = "Выберите файл с выгрузкой по чатам";
             // 
-            // textBoxDirectionForWritting
+            // progressBarWriting
             // 
-            this.textBoxDirectionForWritting.Location = new System.Drawing.Point(94, 498);
-            this.textBoxDirectionForWritting.Name = "textBoxDirectionForWritting";
-            this.textBoxDirectionForWritting.Size = new System.Drawing.Size(209, 20);
-            this.textBoxDirectionForWritting.TabIndex = 2;
-            this.textBoxDirectionForWritting.Text = "Выберите пустой Excel для записи";
-            // 
-            // buttonWrite
-            // 
-            this.buttonWrite.Enabled = false;
-            this.buttonWrite.Location = new System.Drawing.Point(13, 527);
-            this.buttonWrite.Name = "buttonWrite";
-            this.buttonWrite.Size = new System.Drawing.Size(75, 23);
-            this.buttonWrite.TabIndex = 3;
-            this.buttonWrite.Text = "Записать";
-            this.buttonWrite.UseVisualStyleBackColor = true;
-            this.buttonWrite.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonWrite_MouseClick);
-            // 
-            // progressBar
-            // 
-            this.progressBar.Location = new System.Drawing.Point(12, 590);
-            this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(291, 23);
-            this.progressBar.TabIndex = 4;
+            this.progressBarWriting.Location = new System.Drawing.Point(16, 726);
+            this.progressBarWriting.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBarWriting.Name = "progressBarWriting";
+            this.progressBarWriting.Size = new System.Drawing.Size(388, 23);
+            this.progressBarWriting.TabIndex = 4;
             // 
             // richTextBoxAllThemes
             // 
-            this.richTextBoxAllThemes.Location = new System.Drawing.Point(323, 285);
+            this.richTextBoxAllThemes.Location = new System.Drawing.Point(431, 351);
+            this.richTextBoxAllThemes.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBoxAllThemes.Name = "richTextBoxAllThemes";
             this.richTextBoxAllThemes.ReadOnly = true;
-            this.richTextBoxAllThemes.Size = new System.Drawing.Size(261, 328);
+            this.richTextBoxAllThemes.Size = new System.Drawing.Size(347, 403);
             this.richTextBoxAllThemes.TabIndex = 5;
             this.richTextBoxAllThemes.Text = "";
             // 
             // richTextBoxSelectedThemes
             // 
-            this.richTextBoxSelectedThemes.Location = new System.Drawing.Point(323, 40);
+            this.richTextBoxSelectedThemes.Location = new System.Drawing.Point(431, 49);
+            this.richTextBoxSelectedThemes.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBoxSelectedThemes.Name = "richTextBoxSelectedThemes";
-            this.richTextBoxSelectedThemes.Size = new System.Drawing.Size(261, 202);
+            this.richTextBoxSelectedThemes.Size = new System.Drawing.Size(347, 248);
             this.richTextBoxSelectedThemes.TabIndex = 6;
             this.richTextBoxSelectedThemes.Text = "";
             // 
             // checkBoxAllowChangeThemes
             // 
             this.checkBoxAllowChangeThemes.AutoSize = true;
-            this.checkBoxAllowChangeThemes.Location = new System.Drawing.Point(323, 262);
+            this.checkBoxAllowChangeThemes.Location = new System.Drawing.Point(431, 322);
+            this.checkBoxAllowChangeThemes.Margin = new System.Windows.Forms.Padding(4);
             this.checkBoxAllowChangeThemes.Name = "checkBoxAllowChangeThemes";
-            this.checkBoxAllowChangeThemes.Size = new System.Drawing.Size(168, 17);
+            this.checkBoxAllowChangeThemes.Size = new System.Drawing.Size(216, 21);
             this.checkBoxAllowChangeThemes.TabIndex = 7;
             this.checkBoxAllowChangeThemes.Text = "Разрешить редактирование";
             this.checkBoxAllowChangeThemes.UseVisualStyleBackColor = true;
@@ -118,60 +107,97 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(320, 24);
+            this.label1.Location = new System.Drawing.Point(427, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.Size = new System.Drawing.Size(231, 17);
             this.label1.TabIndex = 8;
             this.label1.Text = "Актуальные тематики для поиска";
             // 
             // buttonChooseReadingDirection
             // 
-            this.buttonChooseReadingDirection.Location = new System.Drawing.Point(12, 392);
+            this.buttonChooseReadingDirection.Location = new System.Drawing.Point(16, 388);
+            this.buttonChooseReadingDirection.Margin = new System.Windows.Forms.Padding(4);
             this.buttonChooseReadingDirection.Name = "buttonChooseReadingDirection";
-            this.buttonChooseReadingDirection.Size = new System.Drawing.Size(75, 23);
+            this.buttonChooseReadingDirection.Size = new System.Drawing.Size(100, 28);
             this.buttonChooseReadingDirection.TabIndex = 9;
             this.buttonChooseReadingDirection.Text = "Выбрать";
             this.buttonChooseReadingDirection.UseVisualStyleBackColor = true;
             this.buttonChooseReadingDirection.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonChooseDirection_MouseClick);
             // 
-            // buttonChooseWrittingDirection
-            // 
-            this.buttonChooseWrittingDirection.Location = new System.Drawing.Point(13, 498);
-            this.buttonChooseWrittingDirection.Name = "buttonChooseWrittingDirection";
-            this.buttonChooseWrittingDirection.Size = new System.Drawing.Size(75, 23);
-            this.buttonChooseWrittingDirection.TabIndex = 10;
-            this.buttonChooseWrittingDirection.Text = "Выбрать";
-            this.buttonChooseWrittingDirection.UseVisualStyleBackColor = true;
-            this.buttonChooseWrittingDirection.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonChooseDirection_MouseClick);
-            // 
             // buttonSaveThemes
             // 
-            this.buttonSaveThemes.Location = new System.Drawing.Point(497, 258);
+            this.buttonSaveThemes.Location = new System.Drawing.Point(663, 318);
+            this.buttonSaveThemes.Margin = new System.Windows.Forms.Padding(4);
             this.buttonSaveThemes.Name = "buttonSaveThemes";
-            this.buttonSaveThemes.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveThemes.Size = new System.Drawing.Size(100, 28);
             this.buttonSaveThemes.TabIndex = 11;
             this.buttonSaveThemes.Text = "Сохранить";
             this.buttonSaveThemes.UseVisualStyleBackColor = true;
             this.buttonSaveThemes.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonSaveThemes_MouseClick);
             // 
+            // labelWritingProgress
+            // 
+            this.labelWritingProgress.AutoSize = true;
+            this.labelWritingProgress.Location = new System.Drawing.Point(16, 700);
+            this.labelWritingProgress.Name = "labelWritingProgress";
+            this.labelWritingProgress.Size = new System.Drawing.Size(119, 17);
+            this.labelWritingProgress.TabIndex = 12;
+            this.labelWritingProgress.Text = "Прогресс записи";
+            // 
+            // progressBarAnalyze
+            // 
+            this.progressBarAnalyze.Location = new System.Drawing.Point(16, 661);
+            this.progressBarAnalyze.Name = "progressBarAnalyze";
+            this.progressBarAnalyze.Size = new System.Drawing.Size(388, 23);
+            this.progressBarAnalyze.TabIndex = 13;
+            // 
+            // labelAnalyzeProgress
+            // 
+            this.labelAnalyzeProgress.AutoSize = true;
+            this.labelAnalyzeProgress.Location = new System.Drawing.Point(16, 635);
+            this.labelAnalyzeProgress.Name = "labelAnalyzeProgress";
+            this.labelAnalyzeProgress.Size = new System.Drawing.Size(128, 17);
+            this.labelAnalyzeProgress.TabIndex = 14;
+            this.labelAnalyzeProgress.Text = "Прогресс анализа";
+            // 
+            // progressBarReading
+            // 
+            this.progressBarReading.Location = new System.Drawing.Point(16, 601);
+            this.progressBarReading.Name = "progressBarReading";
+            this.progressBarReading.Size = new System.Drawing.Size(388, 23);
+            this.progressBarReading.TabIndex = 15;
+            // 
+            // labelReadingProgress
+            // 
+            this.labelReadingProgress.AutoSize = true;
+            this.labelReadingProgress.Location = new System.Drawing.Point(16, 575);
+            this.labelReadingProgress.Name = "labelReadingProgress";
+            this.labelReadingProgress.Size = new System.Drawing.Size(120, 17);
+            this.labelReadingProgress.TabIndex = 16;
+            this.labelReadingProgress.Text = "Прогресс чтения";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 625);
+            this.ClientSize = new System.Drawing.Size(795, 769);
+            this.Controls.Add(this.labelReadingProgress);
+            this.Controls.Add(this.progressBarReading);
+            this.Controls.Add(this.labelAnalyzeProgress);
+            this.Controls.Add(this.progressBarAnalyze);
+            this.Controls.Add(this.labelWritingProgress);
             this.Controls.Add(this.buttonSaveThemes);
-            this.Controls.Add(this.buttonChooseWrittingDirection);
             this.Controls.Add(this.buttonChooseReadingDirection);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBoxAllowChangeThemes);
             this.Controls.Add(this.richTextBoxSelectedThemes);
             this.Controls.Add(this.richTextBoxAllThemes);
-            this.Controls.Add(this.progressBar);
-            this.Controls.Add(this.buttonWrite);
-            this.Controls.Add(this.textBoxDirectionForWritting);
+            this.Controls.Add(this.progressBarWriting);
             this.Controls.Add(this.textBoxDirectionForReading);
             this.Controls.Add(this.buttonRead);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
@@ -184,17 +210,19 @@
         #endregion
 
         private System.Windows.Forms.Button buttonRead;
-        public System.Windows.Forms.TextBox textBoxDirectionForReading;
-        public System.Windows.Forms.TextBox textBoxDirectionForWritting;
-        private System.Windows.Forms.Button buttonWrite;
-        public System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox textBoxDirectionForReading;
+        private System.Windows.Forms.ProgressBar progressBarWriting;
         private System.Windows.Forms.RichTextBox richTextBoxAllThemes;
-        public System.Windows.Forms.RichTextBox richTextBoxSelectedThemes;
+        private System.Windows.Forms.RichTextBox richTextBoxSelectedThemes;
         private System.Windows.Forms.CheckBox checkBoxAllowChangeThemes;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonChooseReadingDirection;
-        private System.Windows.Forms.Button buttonChooseWrittingDirection;
         private System.Windows.Forms.Button buttonSaveThemes;
+        private System.Windows.Forms.Label labelWritingProgress;
+        private System.Windows.Forms.ProgressBar progressBarAnalyze;
+        private System.Windows.Forms.Label labelAnalyzeProgress;
+        private System.Windows.Forms.ProgressBar progressBarReading;
+        private System.Windows.Forms.Label labelReadingProgress;
     }
 }
 
---
+++ SpeechChatAnalytics/Logic/DataReceiver.cs
@@ -0,0 +1,77 @@
+﻿using OfficeOpenXml;
+using SpeechChatAnalytics.Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeechChatAnalytics.Logic
+{
+    class DataReceiver
+    {
+        private event SetValuesForAppropriateProgressBar setValuesForReadingProgressBar;
+        private event PerformStep performStep;
+
+        private string[,] matrixWithData;
+        public DataReceiver(string pathForReading, SetValuesForAppropriateProgressBar readingProgress, PerformStep performStep)
+        {
+            this.performStep = performStep;
+            setValuesForReadingProgressBar = readingProgress;
+            OpenExcelFile(pathForReading);
+        }
+
+        private void OpenExcelFile(string pathForReading)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            FileInfo fileInfo = new FileInfo(pathForReading);
+            try
+            {
+                using (ExcelPackage ep = new ExcelPackage(fileInfo))
+                {
+                    ExcelWorksheet ew = ep.Workbook.Worksheets[0];
+                    GetData(ew);
+                }
+            }
+            catch (System.IO.InvalidDataException)
+            {
+                MessageBox.Show("Выбран неверный формат файла, программа" +
+                    "будет аварийно закрыта. Необходимо для чтения и последующей записи" +
+                    "выбирать файлы Excel расширения .xlsx");
+                throw;
+            }
+
+        }
+
+        private void GetData(ExcelWorksheet ew)
+        {
+            try
+            {
+                setValuesForReadingProgressBar(this, new ProgressBarArguments(0, ew.Dimension.End.Row, 0, 1));
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Выбран неверный Excel файл, программа будет закрыта. Необходимо" +
+                    "выбрать Excel файл содержащий выгрузку по чатам.");
+                throw;
+            }
+            matrixWithData = new string[ew.Dimension.End.Column, ew.Dimension.End.Row];
+
+            for (int i = 1; i <= ew.Dimension.End.Row; i++)
+            {
+                for (int j = 1; j <= ew.Dimension.End.Column; j++)
+                {
+                    matrixWithData[j - 1, i - 1] = ew.Cells[i, j].Value == null ? string.Empty : ew.Cells[i, j].Value.ToString();
+                }
+                performStep(this);
+            }
+        }
+
+        public ref string[,] SendResult()
+        {
+            return ref matrixWithData;
+        }
+    }
+}
